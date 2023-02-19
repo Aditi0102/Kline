a71b5c10adcf92bcaf5bd9709d52f383da3a694d
Swagat47 <swagatraj47@gmail.com>
2023-02-19 23:34:10 +0530

    carousel abhi bhi nhi bana

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Carousel() {
+    const slides = [
+        {
+            url: "https://drive.google.com/file/d/14wQLMFP32zvdIlC3TYaHFasLnhEBQTC2/view?usp=sharing",
+        },
+        {
+            url: "https://drive.google.com/file/d/1mfdlPgDbdmIsbGCHwuci-FzxYrk1M5Q9/view?usp=share_link",
+        },
+        {
+            url: "https://drive.google.com/file/d/1wSEVHvc7voQX3kHrQUehZoE1es7qKP5n/view?usp=share_link",
+        },
+        {
+            url: "https://drive.google.com/file/d/1HgI4mD-kqjr5CHPk75cZ41iL6M72teHC/view?usp=share_link",
+        },
+        {
+            url: "https://drive.google.com/file/d/1eJCww96Q_q6BqakZnSgQab3VNBo9bXaT/view?usp=share_link",
+        },
+    ]
+    return (
+    <div>Carousel</div>
+    )
+}
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -6,25 +6,6 @@ import heroBcg2 from '../assets/hero-bcg-2.jpeg';
 // import { RxDotFilled } from "react-icons/rx";
 
 const Hero = () => {
-  const slides = [
-    {
-      url: "https://drive.google.com/file/d/14wQLMFP32zvdIlC3TYaHFasLnhEBQTC2/view?usp=sharing",
-      par: ""
-    },
-    {
-      url: "https://drive.google.com/file/d/1mfdlPgDbdmIsbGCHwuci-FzxYrk1M5Q9/view?usp=share_link",
-    },
-    {
-      url: "https://drive.google.com/file/d/1wSEVHvc7voQX3kHrQUehZoE1es7qKP5n/view?usp=share_link",
-    },
-    {
-      url: "https://drive.google.com/file/d/1HgI4mD-kqjr5CHPk75cZ41iL6M72teHC/view?usp=share_link",
-    },
-    {
-      url: "https://drive.google.com/file/d/1eJCww96Q_q6BqakZnSgQab3VNBo9bXaT/view?usp=share_link",
-    },
-  ]
-
   return (
     <Wrapper className='section-center'>
       <article className='content'>
