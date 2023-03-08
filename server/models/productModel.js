421bbbc5030b92c6bcbba53aa86c609518423b2d
Swagat47 <swagatraj47@gmail.com>
2023-03-08 19:03:30 +0530

    some few changes

---
+++ server/models/productModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength: [100, 'Product name cannot exceed 100 characters']
+    },
+    disciption: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLength: [5, 'Product price cannot exceed 5 characters'],
+        default: 0.0
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this product'],
+        enum: {
+            values: [
+                'Wine Glasses',
+                'Beer Glasses',
+                'Cocktail Glasses',
+                'Coffee Mugs',
+                'Wiskey Glasses',
+                'Accessories',
+                'Outdoor',
+                'Home'
+            ],
+            message: 'Please select correct category for product'
+        }
+    },
+    timestapms: true,
+
+})
+
+module.exports = mongoose.model('Product', productSchema);
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const connectDatabase = require("./config/database");
 dotenv.config({ path: "./config/config.env" });
 
 connectDatabase();
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
