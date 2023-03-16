27feb50109dad967a57bd8b070ee16519cd5e5ac
Aditi0102 <aditiagarwal1001@gmail.com>
2023-03-16 18:53:18 +0530

    ordermodel plus terms

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(cookieParser());
 //Routes Import
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use(cors({
     origin:'*', credentials: true
@@ -17,6 +18,7 @@ app.use(cors({
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //middleware for Errors
 app.use(errorMiddleware);
